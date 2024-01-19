47f74c5d6c088500505569f8aad463f63ac67888
joaosiles <joaosiles@gmail.com>
2024-01-18 23:37:54 -0300

    Cria base do projeto Pokedex utilizando Next.js

---
+++ src/app/components/home.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col">
+      <div className='flex flex-row headerDex'>
+        <div>logo</div>
+        <div>logo 2</div>
+      </div>
+
+      <div className='flex flex-row'>
+        <div>menu</div>
+        <div>content</div>
+      </div>     
+    </main>
+  )
+}
