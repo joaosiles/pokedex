f196e182cb923ccba1e640b94cbf8ba15ba4a7ed
joaosiles <joaosiles@gmail.com>
2024-01-20 19:48:01 -0300

    Finaliza layout dos filtros superiores

--- src/app/components/filter-top/index.js
+++ src/app/components/filter-top/index.js
@@ -5,13 +5,23 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const FilterTop = (props) => {
     return (
-    <div id='searchBox' className='flex justify-between items-center my-5 py-3'>
-        <input
-            type="text"
-            placeholder="Pesquisar por nome ou número..."        
-        />
-        <FontAwesomeIcon className='icon' icon={faSearch} />                
-    </div>
+    <div className='flex flex-nowrap w-full'>
+        <div id='searchBox' className='flex justify-between items-center my-5 py-3'>
+            <input
+                type="text"
+                placeholder="Pesquisar por nome ou número..."
+                id='search'    
+            />
+            <FontAwesomeIcon className='icon' icon={faSearch} />                
+        </div>
+        <div id='selectBox' className='flex flex-row justify-end w-full my-2 py-3'>
+            <label for='selectTop' className='my-2 mr-3'>Ordenar por</label>
+                <select id='selectTop'>
+                <option value="highNumber">Maior número</option>
+                <option value="lowerNumber">Menor número</option>                
+            </select>
+        </div>
+    </div>        
     );
    };
    
