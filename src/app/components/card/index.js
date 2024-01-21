0cc41ef8fbb433a4aa1ffc6f77fdedc7c9c47d74
joaosiles <joaosiles@gmail.com>
2024-01-20 22:42:06 -0300

    Ajusta estilo básico do card de pokémon

--- src/app/components/card/index.js
+++ src/app/components/card/index.js
@@ -3,8 +3,22 @@ import './index.css'
 
 const Card = (props) => {
     return (
-    <div className='flex flex-wrap w-full'>
-        Teste Card
+    <div id='card' className='flex flex-wrap'>
+        <div className='pokemon'>
+            <img
+            src="https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/bulbasaur.png"
+            alt="Bulbasaur"
+                />
+            <div>
+                <div className='number mt-2'>Nº 001</div>
+                <div className='name'>Bulbasaur</div>
+                <div className='type'>
+                    <span className='card-type flex flex-nowrap'>
+                        Grass
+                    </span>
+                </div>
+            </div>    
+        </div>
     </div>        
     );
    };
