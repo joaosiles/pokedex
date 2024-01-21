863393cc2b4fc92401c8d6129efc5581d9b87ff6
joaosiles <joaosiles@gmail.com>
2024-01-20 22:06:43 -0300

    Finaliza layout do menu lateral

--- src/app/components/filter-left/index.js
+++ src/app/components/filter-left/index.js
@@ -1,13 +1,45 @@
-"use client";
-import React, { useState } from 'react';
-import './index.css';
+"use client"
+import React, { useState } from 'react'
+import Switch from '@mui/material/Switch'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './index.css'
 
 const FilterLeft = (props) => {
     const [isChecked, setIsChecked] = useState(false);
+    const [showFavs, setShowFavs] = useState(false);
 
     const handleCheckboxChange = () => {
         setIsChecked(!isChecked);
     };
+
+    const handleFavsChange = () => {
+        setShowFavs(!isChecked);
+    };
+
+    const red =  createTheme({
+        components: {
+          MuiSwitch: {
+            styleOverrides: {
+              switchBase: {                
+                color: "#E1E1E1"
+              },
+              colorPrimary: {
+                "&.Mui-checked": {                  
+                  color: "#E2350D"
+                }
+              },
+              track: {                
+                opacity: 0.2,
+                backgroundColor: "F1F1F1",
+                ".Mui-checked.Mui-checked + &": {                  
+                  opacity: 0.7,
+                  backgroundColor: "#f7dad4"
+                }
+              }
+            }
+          }
+        }
+      });
 
     return (
         <div className='w-full'>
@@ -24,6 +56,14 @@ const FilterLeft = (props) => {
                         onChange={handleCheckboxChange}
                     />
                 </label>
+            </div>
+            <div className='w-full'>
+                Filtrar Favoritos
+            </div>
+            <div className='w-full justify-start'>
+                <ThemeProvider theme={red}>
+                    <Switch/>
+                </ThemeProvider>
             </div>
         </div>
     );
