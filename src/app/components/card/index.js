52f0fb6e615c67d010807f7c246932ba8d7ac215
joaosiles <joaosiles@gmail.com>
2024-01-21 22:55:37 -0300

    cria estilo de favoritos

--- src/app/components/card/index.js
+++ src/app/components/card/index.js
@@ -1,29 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 const Card = (pokemonData) => {
+  const [fav, setFav] = useState(false);
+
+  const favChange = () => {
+    let heart = fav
+    setFav(!heart)    
+  }
 
 return (
     <div id='card' className='flex flex-wrap'>
-        <div className='pokemon'>
-            <img
-              src={pokemonData ? pokemonData.pokemonData.sprites.normal : ''}
-              alt={pokemonData ? pokemonData.pokemonData.name : ''}
-            />
-            <div>
-                <div className='number mt-2'>Nº{pokemonData ? pokemonData.pokemonData.national_number : ''}</div>
-                <div className='name'>{pokemonData ? pokemonData.pokemonData.name : ''}</div>
-                <div className='type'>
-                <span className='card-type flex flex-nowrap'>
-                  {pokemonData.pokemonData.type.map((type, typeIndex) => (
-                    <div key={typeIndex} className={`space-type m-2 ${type}`}>
-                      {type}
-                    </div>
-                  ))}
-                </span>
-              </div>
-            </div>    
-        </div>
+    <div className='pokemon icon-container'> 
+          <FontAwesomeIcon
+            className={`icon ${fav ? 'icon-fav' : ''}`}
+            icon={faHeart}
+            onClick={favChange}            
+          />  
+        <img
+          src={pokemonData ? pokemonData.pokemonData.sprites.normal : ''}
+          alt={pokemonData ? pokemonData.pokemonData.name : ''}
+        />
+        <div>
+            <div className='number mt-2'>Nº{pokemonData ? pokemonData.pokemonData.national_number : ''}</div>
+            <div className='name'>{pokemonData ? pokemonData.pokemonData.name : ''}</div>
+            <div className='type'>
+            <span className='card-type flex flex-nowrap'>
+              {pokemonData.pokemonData.type.map((type, typeIndex) => (
+                <div key={typeIndex} className={`space-type m-2 ${type}`}>
+                  {type}
+                </div>
+              ))}
+            </span>
+          </div>
+        </div>    
+      </div>
     </div>        
   );
 };
